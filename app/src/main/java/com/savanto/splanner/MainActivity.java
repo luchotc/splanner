3f6255d411ffc394d32c644decc4d0b49f91f94b
savanto <savanto@users.noreply.github.com>
2015-08-25 22:01:22 -0700

    Basic layout

---
+++ app/src/main/java/com/savanto/splanner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.savanto.splanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.main_activity);
+    }
+}
